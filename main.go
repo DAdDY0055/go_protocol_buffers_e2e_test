7a7174413ef5a125e00aa02b6b7438a5c500d3bc
Takuya <yky2800.rthm.55tn@gmail.com>
2020-12-06 18:50:01 +0900

    protofileを作成

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	// "github.com/DADDY0055/go_protocol_buffers_e2e_test/proto"
+	"github.com/DADDY0055/go_protocol_buffers_e2e_test/proto"
 	"github.com/labstack/echo/v4"
 )
 
