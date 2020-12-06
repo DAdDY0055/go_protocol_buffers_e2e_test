09308105eba547d2a7213ff47478cfc52c751e3e
Takuya <yky2800.rthm.55tn@gmail.com>
2020-12-06 19:31:36 +0900

    レスポンスをProtocolBuffersにする

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/DADDY0055/go_protocol_buffers_e2e_test/proto"
+	pb "github.com/DAdDY0055/go_protocol_buffers_e2e_test/proto"
 	"github.com/labstack/echo/v4"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		hello := &pb.Hello{Hello: "Hello, World!"}
+		data, _ := proto.Marshal(hello)
+		return c.Blob(http.StatusOK, "application/protobuf", data)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
